e916b5aef37efaf630a58dd01d2709285fe57f01
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-08-26 17:25:43 -0300

    fazendo dois requesitos obrigatorios

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const {species} = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalFiltrado = species.filter((elemento) => {
+    return elemento.name === animal;
+  });
+  
+  const todosAnimaisDaEspecie = animalFiltrado[0].residents;
+
+  return todosAnimaisDaEspecie.every((elemento) => elemento.age >= age);
+  
 }
+
+console.log(getAnimalsOlderThan('lions', 1));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquikdjfk
+const {species} = data;
+
+function getSpeciesByIds(...ids) {
+  
+  return ids.reduce((agrupador, elemento) => {
+    species.forEach((animal) => {
+      if(elemento === animal.id){
+         agrupador.push(animal);
+      }
+    });
+    return agrupador;
+  },[]);
 }
+//onsole.log(species);
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
