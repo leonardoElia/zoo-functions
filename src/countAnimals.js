8b1d376c3ad0d4ba6f967f839b4533003d70200a
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-08-29 17:07:44 -0300

    fazendo 7 requesitos obrigatorios e 1 bonus

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (typeof animal === 'undefined') {
+    const animaisQuantidade = {};
+    species.forEach((elemento) => {
+      animaisQuantidade[elemento.name] = elemento.residents.length;
+    });
+    return animaisQuantidade;
+  }
+  const animalDigitado = animal.specie;
+  const objetoAnimal = species.find((obejeto) => obejeto.name === animalDigitado);
+  if (animal.sex === 'female') {
+    const femenino = objetoAnimal.residents.filter((elemento) => elemento.sex === 'female');
+    return femenino.length;
+  }
+  if (animal.sex === 'male') {
+    const masculino = objetoAnimal.residents.filter((elemento) => elemento.sex === 'male');
+    return masculino.length;
+  }
+  return objetoAnimal.residents.length;
 }
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,4 +1,40 @@
 // const { TestScheduler } = require('jest');
-// const handlerElephants = require('../src/handlerElephants');
+const handlerElephants = require('../src/handlerElephants');
 
-// describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('testando retorno de quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('testando se retorna um array com o nome de todos os elefantes', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+
+  it('testando se retorna a media de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+
+  it('Testando se retorna a localização dos elefantes no zoologico', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+
+  it('Testando se retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+
+  it('testando se retorna um array com os dias onde é possivel visitar elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+
+  it('testando se a função retorna undefined quando não é passado nenhum parametro', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
+
+  it('testando se a função retorna null quando é passado um parametro que não existe', () => {
+    expect(handlerElephants('xablau')).toEqual(null);
+  });
+
+  it('testando se quando passa um numero comoo parametro retorna aviso', () => {
+    expect(handlerElephants(6)).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+});
