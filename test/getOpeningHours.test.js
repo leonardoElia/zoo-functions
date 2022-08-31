b8c24a6c1df6176d8c603adea87214e9f5f5a830
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-08-30 23:34:09 -0300

    acabando o projeto

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,14 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testando caso nenhum parametro é passado', () => {
+    const esperado = getOpeningHours();
+    expect(getOpeningHours()).toEqual(esperado);
+  });
+
+  it('testando dias da semana', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+});
